dc2dd455a310e25cea470b013471b7b5308b6af3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-13 21:19:01 +0300

    recipe class

--- production/models/__init__.py
+++ production/models/__init__.py
@@ -3,4 +3,6 @@ from production.models.goods import *
 from production.models.detail_models import *
 from production.models.country import *
 from production.models.producer import *
+from production.models.material import *
+from production.models.recipe import *
 
---
+++ production/models/recipe.py
@@ -0,0 +1,21 @@
+from django.db import models
+from production.models import MasterBatch, Goods, MaterialType
+
+
+class Recipe(models.Model):
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    main_material_type = models.ForeignKey(MaterialType, on_delete=models.CASCADE)
+    add_material_doze = models.DecimalField(max_digits=4, decimal_places=2)
+    main_master = models.ForeignKey(MasterBatch, on_delete=models.CASCADE, null=True, related_name='main')
+    main_master_doze = models.DecimalField(max_digits=4, decimal_places=2)
+    add_master = models.ForeignKey(MasterBatch, on_delete=models.CASCADE, null=True, related_name='additional')
+    add_master_doze = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __repr__(self):
+        return (f"{self.main_material_type}  дробленка {self.add_material_doze}% краситель1 {self.main_master} "
+                f"доза {self.main_master_doze}% краситель 2 {self.add_master} доза {self.add_material_doze}%")
+
+    def __str__(self):
+        return (f"{self.main_material_type}  дробленка {self.add_material_doze}% краситель1 {self.main_master} "
+                f"доза {self.main_master_doze}% краситель 2 {self.add_master} доза {self.add_material_doze}%")
+

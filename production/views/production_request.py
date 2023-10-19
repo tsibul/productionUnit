46ac215b788305742dc42019529d8384510d75c5
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-19 21:40:02 +0300

    production request ready except search

--- production/views/production_request.py
+++ production/views/production_request.py
@@ -8,9 +8,9 @@ from production.models import ProductionRequest, DetailInGoods, Color
 def production_request(request):
     navi = 'request'
     product_request = ProductionRequest.objects.filter(deleted=False).order_by('-date_create', 'detail__goods__name')[
-                      0:19]
+                      0:11]
     product_request_end = ProductionRequest.objects.filter(deleted=False).order_by('-date_create',
-                                                                                   'detail__goods__name')[19:20]
+                                                                                   'detail__goods__name')[11:12]
     color = Color.objects.filter(deleted=False).order_by('color_scheme', 'color_id')
     detail = DetailInGoods.objects.filter(deleted=False).order_by('goods', 'position')
     context = {'navi': navi, 'product_request': product_request, 'product_request_end': product_request_end,

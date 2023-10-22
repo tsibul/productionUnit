519548968c3ce67518a7b459ac4a3e331a126b61
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-22 13:38:39 +0300

    production initial commit

--- production/views/__init__.py
+++ production/views/__init__.py
@@ -2,3 +2,4 @@ from production.views.main import *
 from production.views.dictionary import *
 from production.views.login import *
 from production.views.production_request import *
+from production.views.production import *

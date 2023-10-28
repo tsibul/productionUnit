91793f6d49c95b004917c544b82c2f95b72879af
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-28 19:49:46 +0300

    add QualityForRequest model

--- production/models/quality_report.py
+++ production/models/quality_report.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-from production.models import ProductionReport, Defects, DefectEvent
+from production.models import ProductionReport, Defects, DefectEvent, ProductionRequest
 
 
 class QualityReport(models.Model):
@@ -24,6 +24,17 @@ class QualityReportDefects(models.Model):
     quality_report = models.ForeignKey(QualityReport, on_delete=models.CASCADE)
     defect = models.ForeignKey(Defects, models.SET_NULL, null=True)
     deleted = models.BooleanField(default=False)
+
+    @staticmethod
+    def order_default():
+        return ['-quality_report__date_check']
+
+
+class QualityForRequest(models.Model):
+    quality_report = models.ForeignKey(QualityReport, on_delete=models.CASCADE)
+    production_request = models.ForeignKey(ProductionRequest, models.SET_NULL, null=True)
+    quantity_checked = models.IntegerField(default=0)
+    quantity_approved = models.IntegerField(default=0)
 
     @staticmethod
     def order_default():

f68d9ebb78e0c27f48d624ca65ed9004c1c157a0
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-28 19:56:07 +0300

    split def production_request to 3 functions added spread_production_for_requests + technical_request_create

--- production/views/main.py
+++ production/views/main.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 
 from production.models import IMM, DetailInGoods, Color, ProductionRequest, ProductionForRequest, \
     ProductionRequestStartStop, ProductionReport
-from production.classes import TotalRequests
+from production.classes import TotalRequest
 
 
 def index(request):
@@ -26,11 +26,11 @@ def production_state(request, in_work):
                   .values_list('detail', 'color').distinct())
     requests = []
     for item in on_request:
-        total_request = TotalRequests(item)
+        total_request = TotalRequest(item)
         if in_work and total_request.imm_id:
-            requests.append(TotalRequests(item))
+            requests.append(TotalRequest(item))
         elif not in_work and not total_request.imm_id:
-            requests.append(TotalRequests(item))
+            requests.append(TotalRequest(item))
     serialized_requests = [request.__dict__ for request in requests]
     serialized_requests = sorted(serialized_requests, key=lambda order: (order['detail'], order['first_date']))
     json_response = json.dumps(serialized_requests, ensure_ascii=False)
@@ -74,8 +74,16 @@ def production_report(request):
     production = ProductionReport(detail=detail, color=color, date=date_now, imm=imm, user=current_user,
                                   quantity=quantity)
     production.save()
-    production_request = (ProductionRequest.objects.filter(deleted=False, closed=False, color=color, detail=detail)
-                          .order_by('date_create'))
+    quantity_left = spread_production_for_requests(production, quantity)
+    if quantity_left > 0:
+        technical_request_create(quantity_left, production)
+    return HttpResponseRedirect(reverse('production:main'))
+
+
+def spread_production_for_requests(production, quantity):
+    production_request = (
+        ProductionRequest.objects.filter(deleted=False, closed=False, color=production.color,
+                                         detail=production.detail).exclude(quantity_left=0).order_by('date_create'))
     for i, pr_request in enumerate(production_request):
         production_for_request = ProductionForRequest.objects.filter(production_request=pr_request)
         quantity_produced = production_for_request.aggregate(quantity_produced=Sum('quantity'))['quantity_produced']
@@ -98,31 +106,38 @@ def production_report(request):
             pr_request.quantity_left = 0
             pr_request.save()
             quantity = quantity - pr_request.quantity + quantity_produced
-            if quantity == 0:
-                break
-            production_start_stop = ProductionRequestStartStop.objects.get(production_request=pr_request, imm=imm,
+            production_start_stop = ProductionRequestStartStop.objects.get(production_request=pr_request,
+                                                                           imm=production.imm,
                                                                            date_stop=None)
-            production_start_stop.date_stop = date_now
-            production_start_stop.user_stop = current_user
+            production_start_stop.date_stop = production.date
+            production_start_stop.user_stop = production.user
             production_start_stop.stop_reason = 'заказ выполнен'
             production_start_stop.save()
+            if quantity == 0:
+                break
             if i + 1 == production_request.count():
                 break
             pr_request_next = production_request[i + 1]
             production_start_stop_new = ProductionRequestStartStop(production_request=pr_request_next,
-                                                                   date_start=date_now, user_start=current_user,
-                                                                   imm=imm)
+                                                                   date_start=production.date,
+                                                                   user_start=production.user,
+                                                                   imm=production.imm)
             production_start_stop_new.save()
-    if quantity > 0:
-        technical_request = ProductionRequest(detail=detail, color=color, quantity=quantity, quantity_left=0,
-                                              date_create=date_now, user=current_user, if_order=False, technical=True)
-        technical_request.save()
-        technical_request_start_stop = ProductionRequestStartStop(production_request=technical_request,
-                                                                  date_start=date_now, user_start=current_user, imm=imm,
-                                                                  date_stop=date_now, user_stop=current_user,
-                                                                  stop_reason='технический заказ выполнен')
-        technical_request_start_stop.save()
-        technical_request_production_for_request = ProductionForRequest(production=production, quantity=quantity,
-                                                                        production_request=technical_request)
-        technical_request_production_for_request.save()
-    return HttpResponseRedirect(reverse('production:main'))
+    return quantity
+
+
+def technical_request_create(quantity, production):
+    technical_request = ProductionRequest(detail=production.detail, color=production.color, quantity=quantity,
+                                          quantity_left=0,
+                                          date_create=production.date, user=production.user, if_order=False,
+                                          technical=True)
+    technical_request.save()
+    technical_request_start_stop = ProductionRequestStartStop(production_request=technical_request,
+                                                              date_start=production.date, user_start=production.user,
+                                                              imm=production.imm,
+                                                              date_stop=production.date, user_stop=production.user,
+                                                              stop_reason='технический заказ выполнен')
+    technical_request_start_stop.save()
+    technical_request_production_for_request = ProductionForRequest(production=production, quantity=quantity,
+                                                                    production_request=technical_request)
+    technical_request_production_for_request.save()

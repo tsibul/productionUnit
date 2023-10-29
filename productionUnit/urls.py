ec8322dcb7321645995b33b6a540bb8e87505c55
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-29 22:38:21 +0300

    changed admin url

--- productionUnit/urls.py
+++ productionUnit/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin_production/', admin.site.urls),
     path('production/', include('production.urls')),
 ]

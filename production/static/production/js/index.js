1b3a6352e33b5b5de212c8e7b7d003f7c1246d73
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-11-01 23:49:41 +0300

    bug fix if there's no quantity left

--- production/static/production/js/index.js
+++ production/static/production/js/index.js
@@ -83,7 +83,8 @@ async function fillProductionData(row, element, prefix) {
     fillFields(row,element,prefix);
     row.dataset.detail = element['detail_id'];
     row.dataset.color = element['color_id'];
-    if(row.querySelector('.req__left').textContent === '0'){
+    const reqLeft = row.querySelector('.req__left')
+    if(reqLeft && reqLeft.textContent === '0'){
         row.querySelector('.btn').classList.add('form-input__inactive');
         row.querySelector('.btn').disabled = true;
     }

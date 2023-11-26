5cbd554c20e451e7c733c9daa200770fc558d0f5
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-11-26 22:21:24 +0300

    Add docstring

--- production/service_function/authentificition.py
+++ production/service_function/authentificition.py
@@ -1,4 +1,8 @@
 def user_group_list(request):
+    """ return list of user groups for current user \n
+    :param request:
+    :return: list of user groups for current user as a string
+    """
     groups = request.user.groups.values_list('name', flat=True)
     user_groups = ', '.join(groups) if groups else ''
     return user_groups

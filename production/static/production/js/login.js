56fcb614a94d882521f70fe3175ff4c8c5ef0143
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-12-30 22:06:42 +0300

    remove openModal && closeModel into modules

--- production/static/production/js/login.js
+++ production/static/production/js/login.js
@@ -1,22 +1,12 @@
+import {closeModal} from "./func/closeModal.js";
+import {openModal} from "./func/openModal.js";
+
 // logout_modal.js
 const authModal = document.getElementById('authModal');
 const closeButtons = document.querySelectorAll('.login__close');
 const loginForm = document.getElementById('login-form');
 const logoutForm = document.getElementById('logout-form');
 
-
-function openModal(modal) {
-    modal.style.display = 'block';
-}
-
-function closeModal(click) {
-    const modal = click.closest('.login');
-    const inputs = modal.querySelectorAll('input');
-    const textAreas = modal.querySelectorAll('textarea');
-    [...inputs].forEach(e => {e.value = null});
-    [...textAreas].forEach(e => {e.textContent = ''});
-    modal.style.display = 'none';
-}
 
 closeButtons.forEach(row => {
     row.addEventListener("click", (e) => {

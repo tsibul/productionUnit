e28ae173618146f65bf583ae269a6289aedb1c6b
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-06 01:43:09 +0300

    changed to async

--- production/static/production/js/quality.js
+++ production/static/production/js/quality.js
@@ -17,17 +17,16 @@ const modalCloseButton = qualityModal.querySelector('.btn-close');
 const modalSaveButton = qualityModal.querySelector('.btn-save');
 
 
-reportButton.addEventListener('click', e => {
+reportButton.addEventListener('click', async e => {
     e.preventDefault();
     productionContent.innerHTML = '';
-    addProducedRows(startDate.value, endDate.value).then(r => {
-        sectionProduction.querySelectorAll('.qual__row').forEach(row => {
-            row.addEventListener('click', (event) => {
-                if (event.target.classList.contains('btn')) {
-                    fillQualityModal(event.target);
-                    openModal(qualityModal);
-                }
-            });
+    await addProducedRows(startDate.value, endDate.value);
+    sectionProduction.querySelectorAll('.qual__row').forEach(row => {
+        row.addEventListener('click', async (event) => {
+            if (event.target.classList.contains('btn')) {
+                await fillQualityModal(event.target);
+                openModal(qualityModal);
+            }
         });
     });
 })
@@ -57,7 +56,7 @@ modalSaveButton.addEventListener('click', e => {
     fetch(fetchPath, {
         method: 'POST',
         body: formData,
-    }).then(r =>   closeModal(modalCloseButton));
+    }).then(r => closeModal(modalCloseButton));
 });
 
 modalCloseButton.addEventListener('click', () => {
@@ -86,9 +85,7 @@ async function fillQualityModal(btn) {
         rowCur.querySelector('.qual__comment').textContent;
     const defectBlock = qualityModal.querySelector('.modal__block');
     defectBlock.innerHTML = '';
-    const defectData = await fetch(`/production/defects_left/${rowCur.dataset.id}`)
-        .then(response => response.json());
-
+    const defectData = await fetch(`/production/defects_left/${rowCur.dataset.id}`);
     const defectList = JSON.parse(defectData);
     defectList.forEach(defect => {
         defectBlock.insertAdjacentHTML("afterbegin",

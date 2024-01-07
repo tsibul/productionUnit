7b0a9c752935f406bd3161555bb0facf6078c10e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-07 20:39:39 +0300

    change order for Recpe

--- production/models/__init__.py
+++ production/models/__init__.py
@@ -4,11 +4,11 @@ from production.models.detail_models import DetailInGoods, DetailName
 from production.models.country import Country
 from production.models.producer import Producer
 from production.models.material import AddMaterial, MaterialType, MainMaterial, MasterBatch
-from production.models.recipe import Recipe
 from production.models.injection_molding_machine import IMM
 from production.models.defects import Defects, DefectEvent
 from production.models.production_request import ProductionRequest, ProductionRequestStartStop
 from production.models.production_report import ProductionReport, ProductionForRequest
 from production.models.quality_report import QualityReport, QualityReportDefects, QualityForRequest
+from production.models.recipe import Recipe
 
 

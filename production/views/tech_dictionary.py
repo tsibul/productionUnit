0ddc65bf5f9efb61748518ad6ae7080e3074ccfb
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-08 10:38:56 +0300

    add recipe_color_scheme & recipe detail to view

--- production/views/tech_dictionary.py
+++ production/views/tech_dictionary.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render
-from production.models import DetailInGoods, Recipe, IMM
+from django.utils import timezone
+
+from production.models import DetailInGoods, Recipe, RecipeDetail, IMM, RecipeColorScheme
 from production.service_function import user_group_list, if_admin, badges
 
 
 def tech_dictionary(request):
     """
-    Return first 20 records of each dictionary, sorted by default (for static page)
+    Return first 20 records of each tech_dictionary, sorted by default (for static page)
     :param request:
     :return:
     """
     navi = 'tech_dictionary'
+    date_now = timezone.now()
     admin_state = if_admin(request)
     detail_in_goods = DetailInGoods.objects.filter(deleted=False).order_by('goods__article', 'position')[0:19]
     detail_in_goods_end = DetailInGoods.objects.filter(deleted=False).order_by('goods__article', 'position')[19:20]
-    recipe = Recipe.objects.filter(deleted=False).order_by('goods__article')[0:19]
-    recipe_end = Recipe.objects.filter(deleted=False).order_by('goods__article')[19:20]
+    recipe_color_scheme = RecipeColorScheme.objects.filter(deleted=False).order_by('color_scheme')
+    recipe = Recipe.objects.filter(deleted=False).order_by('recipe_color_scheme', 'color')[0:19]
+    recipe_end = Recipe.objects.filter(deleted=False).order_by('recipe_color_scheme', 'color')[19:20]
+    recipe_detail = RecipeDetail.objects.filter(deleted=False).order_by('recipe_color_scheme', 'color')[0:19]
+    recipe_detail_end = RecipeDetail.objects.filter(deleted=False).order_by('recipe_color_scheme', 'color')[19:20]
     imm = IMM.objects.filter(deleted=False).order_by('plant_code')
     user_groups = user_group_list(request)
     badge_count = badges()
     context = {'navi': navi, 'detail_in_goods': detail_in_goods, 'detail_in_goods_end': detail_in_goods_end,
                'imm': imm, 'recipe': recipe, 'recipe_end': recipe_end, 'user_groups': user_groups,
-               'admin_state': admin_state, 'badge_count': badge_count}
+               'admin_state': admin_state, 'badge_count': badge_count, 'recipe_color_scheme': recipe_color_scheme,
+               'recipe_detail': recipe_detail, 'date_now': date_now, 'recipe_detail_end': recipe_detail_end}
     return render(request, 'tech_dictionary.html', context)
 
 

9a7258864531ebbe90a91df193cd681bcefcbe2d
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-01-08 15:27:13 +0300

    Change base weight precision, change field names

--- production/models/detail_weight.py
+++ production/models/detail_weight.py
@@ -3,23 +3,23 @@ from production.models import DetailInGoods, ProductionReport
 
 
 class DetailBaseWeight(models.Model):
-    detail = models.ForeignKey(DetailInGoods, on_delete=models.CASCADE)
-    weightG = models.DecimalField(default=0, decimal_places=1, max_digits=6)
+    detail_in_goods = models.ForeignKey(DetailInGoods, on_delete=models.CASCADE)
+    weight_g = models.DecimalField(default=0, decimal_places=3, max_digits=9)
     date_create = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
 
     def __repr__(self):
-        return self.weightG
+        return self.weight_g
 
     def __str__(self):
-        return str(self.weightG)
+        return str(self.weight_g)
 
     class Meta:
-        ordering = ['detail', '-date_create']
+        ordering = ['detail_in_goods', '-date_create']
 
     @staticmethod
     def order_default():
-        return ['detail', '-date_create']
+        return ['detail_in_goods', '-date_create']
 
 
 class DetailActualWeight(DetailBaseWeight):
